2d95d7de4b72d55e4362ab4f24691d375928636e
NoStackDev <partialstackdev@gmail.com>
2023-05-19 17:18:48 +0100

    minor changes

--- src/section/Products.tsx
+++ src/section/Products.tsx
@@ -34,11 +34,11 @@ const Products = React.forwardRef<React.ElementRef<"section">, Props>(
         </Button>
 
         <div className="flex mt-4 md:mt-6 gap-3">
-          <div className="px-3 py-[5px] border-[1px] border-secondary text-title_3 font-body font-medium bg-secondary text-white hover:cursor-pointer">1</div>
-          <div className="px-3 py-[5px] border-[1px] border-secondary text-title_3 font-body font-medium hover:cursor-pointer">2</div>
-          <div className="px-3 py-[5px] border-[1px] border-secondary text-title_3 font-body font-medium hover:cursor-pointer">3</div>
-          <div className="px-3 py-[5px] border-[1px] border-secondary text-title_3 font-body font-medium hover:cursor-pointer">4</div>
-          <div className="px-3 py-[5px] border-[1px] border-secondary text-title_3 font-body font-medium hover:cursor-pointer">5</div>
+          <div className="px-2 py-[5px] border-[1px] border-secondary text-title_3 font-body font-medium bg-secondary text-white hover:cursor-pointer">1</div>
+          <div className="px-2 py-[5px] border-[1px] border-secondary text-title_3 font-body font-medium hover:cursor-pointer">2</div>
+          <div className="px-2 py-[5px] border-[1px] border-secondary text-title_3 font-body font-medium hover:cursor-pointer">3</div>
+          <div className="px-2 py-[5px] border-[1px] border-secondary text-title_3 font-body font-medium hover:cursor-pointer">4</div>
+          <div className="px-2 py-[5px] border-[1px] border-secondary text-title_3 font-body font-medium hover:cursor-pointer">5</div>
           <div className="px-2 py-[5px] border-[1px] border-secondary text-title_3 font-body font-medium hover:cursor-pointer md:ml-6">100</div>
           <div className="px-2 py-[5px] border-[1px] border-secondary text-title_3 font-body font-medium hover:cursor-pointer">101</div>
         </div>
