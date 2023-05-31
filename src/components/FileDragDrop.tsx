7a2710f1097c5f4e8741500d02c1a879bd44ea79
NoStackDev <partialstackdev@gmail.com>
2023-05-31 19:32:02 +0100

    still working on request form

--- src/components/FileDragDrop.tsx
+++ src/components/FileDragDrop.tsx
@@ -1,28 +1,141 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import React from "react";
+import React, { DragEvent, MouseEvent } from "react";
 
 const ImageIcon = React.lazy(() => import("@mui/icons-material/Image"));
+const DeleteIcon = React.lazy(() => import("@mui/icons-material/Delete"));
 
 const FileDragDrop = React.forwardRef<
   React.ElementRef<"div">,
-  React.ComponentPropsWithoutRef<"div">
->(({ children, className, ...props }, fowardref) => {
-  const [images, setImages] = React.useState<any>([]);
+  React.ComponentPropsWithoutRef<"div"> & {
+    images: {
+      name: string;
+      url: string;
+    }[];
+    setImages: React.Dispatch<
+      React.SetStateAction<
+        {
+          name: string;
+          url: string;
+        }[]
+      >
+    >;
+  }
+>(({ children, className, images, setImages, ...props }, fowardref) => {
   const [isDragging, setIsDragging] = React.useState(false);
   const fileInputRef = React.useRef<HTMLInputElement>(null);
 
+  const selectFiles = () => {
+    console.log("clicked to select files");
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
+  const onFileSelect = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log("file selected");
+    event.preventDefault();
+    const files = event.target.files;
+    console.log("file selected: ", files);
+    if (!files || files?.length === 0) return;
+
+    // for (let i = 0; i < files.length; i++) {
+    //   if (files[i].type.split("/")[0] !== "image") continue;
+    //   if (!images.some((e) => e.name === files[i].name)) {
+    //     setImages([
+    //       ...images,
+    //       { name: files[i].name, url: URL.createObjectURL(files[i]) },
+    //     ]);
+    //   }
+    // }
+
+    setImages([{ name: files[0].name, url: URL.createObjectURL(files[0]) }]);
+  };
+
+  const deleteImage = (event: MouseEvent) => {
+    event.stopPropagation();
+    setImages([]);
+    console.log("deleted image");
+  };
+
+  const onDrop = (event: DragEvent) => {
+    event.preventDefault();
+    setIsDragging(false);
+    const files = event.dataTransfer.files;
+    console.log("sssssssss: ", files);
+
+    // for (let i = 0; i < files.length; i++) {
+    //   if (files[i].type.split("/")[0] !== "image") continue;
+    //   if (!images.some((e) => e.name === files[i].name)) {
+    //     setImages([
+    //       ...images,
+    //       { name: files[i].name, url: URL.createObjectURL(files[i]) },
+    //     ]);
+    //   }
+    // }
+
+    // setImages([{ name: files[0].name, url: URL.createObjectURL(files[0]) }]);
+  };
+
+  const onDragOver = (event: DragEvent) => {
+    event.preventDefault();
+    setIsDragging(true);
+    event.dataTransfer.dropEffect = "copy";
+  };
+
+  const onDragLeave = (event: DragEvent) => {
+    event.preventDefault();
+    setIsDragging(false);
+  };
   return (
     <div
-      ref={fowardref}
       className={cn(
         "relative w-[100px] h-[100px] rounded-lg border-[1px] border-stroke bg-faded hover:cursor-pointer flex items-center justify-center",
         className
       )}
-      {...props}
+      onClick={selectFiles}
+      onDrop={onDrop}
+      onDragOver={onDragOver}
+      onDragLeave={onDragLeave}
+      ref={fowardref}
     >
-      <input type="file" name="" id="" ref={fileInputRef} className="hidden"/>
+      {images.length > 0 ? (
+        images.map((image, index) => {
+          return (
+            <div className="relative w-[100px] h-[100px]" key={index}>
+              <img
+                src={image.url}
+                alt={image.name}
+                className="h-[100px] w-fit"
+              />
+              <React.Suspense
+                fallback={
+                  <div className="absolute top-0 right-0 w-4 h-4 bg-stroke/80"></div>
+                }
+              >
+                <DeleteIcon
+                  className="absolute top-0 right-0 text-stroke w-4 h-4"
+                  onClick={deleteImage}
+                />
+              </React.Suspense>
+            </div>
+          );
+        })
+      ) : (
+        <React.Suspense>
+          <ImageIcon className="text-stroke w-[28px] h-[28px]" />
+        </React.Suspense>
+      )}
+
+      <input
+        type="file"
+        name=""
+        id=""
+        className="hidden"
+        ref={fileInputRef}
+        onChange={onFileSelect}
+      />
     </div>
   );
 });
--- src/components/RequestResponseForm.tsx
+++ src/components/RequestResponseForm.tsx
@@ -1,11 +1,17 @@
 "use client";
 
-import React, { DragEvent, MouseEvent } from "react";
+import React from "react";
 import * as FormPrimitive from "@radix-ui/react-form";
+import * as RadioGroupPrimitive from '@radix-ui/react-radio-group';
 import FileDragDrop from "./FileDragDrop";
 
-const ImageIcon = React.lazy(() => import("@mui/icons-material/Image"));
-const DeleteIcon = React.lazy(() => import("@mui/icons-material/Delete"));
+const LocationOnIcon = React.lazy(
+  () => import("@mui/icons-material/LocationOn")
+);
+const AttachMoneyIcon = React.lazy(
+  () => import("@mui/icons-material/AttachMoney")
+);
+const WhatsAppIcon = React.lazy(() => import("@mui/icons-material/WhatsApp"));
 
 const RequestResponseForm = React.forwardRef<
   React.ElementRef<typeof FormPrimitive.Root>,
@@ -14,42 +20,9 @@ const RequestResponseForm = React.forwardRef<
   const [images, setImages] = React.useState<{ name: string; url: string }[]>(
     []
   );
-  const [isDragging, setIsDragging] = React.useState(false);
-  const fileInputRef = React.useRef<HTMLInputElement>(null);
-
-  const selectFiles = () => {
-    console.log("clicked to select files");
-    if (fileInputRef.current) {
-      fileInputRef.current.click();
-    }
-  };
-
-  const onFileSelect = (event: React.ChangeEvent<HTMLInputElement>) => {
-    console.log("file selected");
-    event.preventDefault();
-    const files = event.target.files;
-    console.log("file selected: ", files);
-    if (!files || files?.length === 0) return;
-
-    // for (let i = 0; i < files.length; i++) {
-    //   if (files[i].type.split("/")[0] !== "image") continue;
-    //   if (!images.some((e) => e.name === files[i].name)) {
-    //     setImages([
-    //       ...images,
-    //       { name: files[i].name, url: URL.createObjectURL(files[i]) },
-    //     ]);
-    //   }
-    // }
-
-    setImages([{ name: files[0].name, url: URL.createObjectURL(files[0]) }]);
-  };
-
-  const deleteImage = (event: MouseEvent) => {
-    event.stopPropagation();
-    setImages([]);
-  };
 
   console.log("images: ", images);
+
   return (
     <FormPrimitive.Root className="py-8 flex flex-col items-center bg-white">
       <div className="font-headline text-headline_3 font-bold text-black px-5 pb-2 border-b-[1px] border-grey/20 w-full text-center">
@@ -64,6 +37,7 @@ const RequestResponseForm = React.forwardRef<
           <FormPrimitive.Label className="font-body text-title_3 font-medium">
             Comment
           </FormPrimitive.Label>
+
           <FormPrimitive.Message match={"valueMissing"}></FormPrimitive.Message>
         </div>
 
@@ -81,55 +55,57 @@ const RequestResponseForm = React.forwardRef<
         className="px-5 mt-8 flex flex-col items-center gap-2 w-full"
       >
         <FormPrimitive.Control asChild>
-          <div
-            className="relative w-[100px] h-[100px] rounded-lg border-[1px] border-stroke bg-faded hover:cursor-pointer flex items-center justify-center"
-            onClick={selectFiles}
-          >
-            {images.length > 0 ? (
-              images.map((image, index) => {
-                return (
-                  <div className="relative w-[100px] h-[100px]" key={index}>
-                    <img
-                      src={image.url}
-                      alt={image.name}
-                      className="h-[100px] w-fit"
-                    />
-                    <React.Suspense
-                      fallback={
-                        <div className="absolute top-0 right-0 w-4 h-4 bg-stroke/80"></div>
-                      }
-                    >
-                      <DeleteIcon
-                        className="absolute top-0 right-0 text-stroke w-4 h-4"
-                        onClick={deleteImage}
-                      />
-                    </React.Suspense>
-                  </div>
-                );
-              })
-            ) : (
-              <React.Suspense>
-                <ImageIcon className="text-stroke w-[28px] h-[28px]" />
-              </React.Suspense>
-            )}
-
-            <input
-              type="file"
-              name=""
-              id=""
-              className="hidden"
-              ref={fileInputRef}
-              onChange={onFileSelect}
-            />
-          </div>
+          <FileDragDrop images={images} setImages={setImages} />
         </FormPrimitive.Control>
 
         <div className="flex items-baseline justify-between">
           <FormPrimitive.Label className="font-body text-title_3 font-medium">
-            <span>Add Image</span> <span>(optional)</span>
+            <span className="font-body text-title_3 font-medium text-[#000000]">
+              Add Image
+            </span>{" "}
+            <span className="font-body text-body_2 font-light text-[#000000]">
+              (optional)
+            </span>
           </FormPrimitive.Label>
         </div>
       </FormPrimitive.Field>
+
+      <FormPrimitive.Field name="location" className="relative h-fit mt-6">
+        <FormPrimitive.Message match={"valueMissing"}></FormPrimitive.Message>
+
+        <FormPrimitive.Control asChild>
+          <input
+            type="text"
+            placeholder="Your location"
+            className="font-body text-body_1 text-[#000000]/60 bg-faded pl-12 py-2 h-full w-full rounded-[4px]"
+            required
+          />
+        </FormPrimitive.Control>
+        <LocationOnIcon className="absolute top-1/2 -translate-y-1/2 left-4 text-[#424040] w-[19.89px] h-[25.11px]" />
+      </FormPrimitive.Field>
+
+      <FormPrimitive.Field name="price" className="relative h-fit mt-4">
+        <FormPrimitive.Control asChild>
+          <input
+            type="text"
+            placeholder="Price (optional)"
+            className="font-body text-body_1 text-[#000000]/60 bg-faded pl-12 py-2 h-full w-full rounded-[4px]"
+          />
+        </FormPrimitive.Control>
+        <AttachMoneyIcon className="absolute top-1/2 -translate-y-1/2 left-4 text-[#424040] w-[19.89px] h-[25.11px]" />
+      </FormPrimitive.Field>
+
+      <FormPrimitive.Field name="Whatsapp number" className="relative h-fit mt-4">
+        <FormPrimitive.Control asChild>
+          <input
+            type="text"
+            placeholder="WhatsApp number"
+            className="font-body text-body_1 text-[#000000]/60 bg-faded pl-12 py-2 h-full w-full rounded-[4px]"
+          />
+        </FormPrimitive.Control>
+        <WhatsAppIcon className="absolute top-1/2 -translate-y-1/2 left-4 text-[#424040] w-[19.89px] h-[25.11px]" />
+      </FormPrimitive.Field>
+
 
       {/* <FormPrimitive.Submit asChild className="px-5">
         <button>Post question</button>
