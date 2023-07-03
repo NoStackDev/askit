1c542681f8c444cc56d27d5e875677b3825a420f
NoStackDev <partialstackdev@gmail.com>
2023-07-03 14:42:46 +0100

    scrollbar scroll

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -49,6 +49,11 @@ const Sidebar = React.forwardRef<HTMLDivElement, Props>(
       }
     }, [showSidebar]);
 
+    const onClickSidebarItem = (eventRef: string) => {
+      console.log(eventRef);
+      setShowSidebar(false);
+    };
+
     return (
       <>
         {showSidebar && (
@@ -57,91 +62,99 @@ const Sidebar = React.forwardRef<HTMLDivElement, Props>(
 
         <div
           className={cn(
-            "px-5 py-10 bg-[#2E2775] flex flex-col gap-5 h-fit md:rounded-[20px] md:ml-[100px] md:mt-14 md:w-[300px]",
+            "px-5 bg-[#2E2775] h-fit md:rounded-[20px] md:ml-[100px] md:mt-14 md:w-[300px]",
             !showSidebar && "hidden md:flex",
-            showSidebar &&
-              "fixed top-0 w-screen max-w-[300px] z-50 overflow-auto",
+            showSidebar && "fixed top-0 w-screen max-w-[300px] z-50",
             className
           )}
           ref={sidebarRef}
         >
-          {sidebarConfig1.map((sidebarItem) => {
-            return (
-              <div
-                className={cn(
-                  "border border-white rounded-lg overflow-clip w-full"
-                )}
-                key={sidebarItem.id}
-              >
-                {sidebarItem.href ? (
-                  <Link
-                    href={sidebarItem.href}
-                    className={cn(
-                      "block w-full px-4 py-2 hover:bg-white hover:cursor-pointer font-body font-medium text-white text-[18px] hover:text-black",
-                      "/" + pathUrl === sidebarItem.href &&
-                        "bg-white text-black"
-                    )}
-                  >
-                    {sidebarItem.title}
-                  </Link>
-                ) : (
-                  <div className="text-white font-body font-medium w-full px-4 mt-4">
-                    {sidebarItem.title}
-                  </div>
-                )}
+          <div className="py-10 h-fit max-h-screen overflow-auto no-scrollbar w-full flex flex-col gap-5">
+            {sidebarConfig1.map((sidebarItem) => {
+              return (
+                <div
+                  className={cn(
+                    "w-full",
+                    !sidebarItem.href &&
+                      "border border-white rounded-lg w-full",
+                    showSidebar && "last:pb-10"
+                  )}
+                  key={sidebarItem.id}
+                >
+                  {sidebarItem.href ? (
+                    <Link
+                      href={sidebarItem.href}
+                      className={cn(
+                        "block border border-white rounded-lg w-full px-4 py-2 hover:bg-white hover:cursor-pointer font-body font-medium text-white text-[18px] hover:text-black",
+                        "/" + pathUrl === sidebarItem.href &&
+                          "bg-white text-black"
+                      )}
+                      onClick={(e) => setShowSidebar(false)}
+                    >
+                      {sidebarItem.title}
+                    </Link>
+                  ) : (
+                    <div className="text-white font-body font-medium w-full px-4 mt-4">
+                      {sidebarItem.title}
+                    </div>
+                  )}
 
-                {sidebarItem.children && (
-                  <Accordion
-                    type="single"
-                    collapsible
-                    className={cn("flex flex-col gap-3 items-start mt-3")}
-                  >
-                    {sidebarItem.children.map((sidebarItemChild) => {
-                      return (
-                        <AccordionItem
-                          value={sidebarItemChild.id}
-                          key={sidebarItemChild.id}
-                          className="w-full"
-                        >
-                          <AccordionTrigger
-                            icon={
-                              <React.Suspense
-                                fallback={
-                                  <div className="w-4 h-4 bg-stroke/60 animate-pulse"></div>
-                                }
-                              >
-                                <ExpandMoreIcon className="text-white/50 group-hover:text-black/50 ease-[cubic-bezier(0.87,_0,_0.13,_1)] transition-transform duration-300 group-data-[state=open]:rotate-180" />
-                              </React.Suspense>
-                            }
-                            headerClassName="w-full"
-                            className="px-4 py-2 group hover:cursor-pointer hover:bg-white w-full flex items-center justify-between"
+                  {sidebarItem.children && (
+                    <Accordion
+                      type="single"
+                      collapsible
+                      className={cn("flex flex-col gap-3 items-start mt-3")}
+                    >
+                      {sidebarItem.children.map((sidebarItemChild) => {
+                        return (
+                          <AccordionItem
+                            value={sidebarItemChild.id}
+                            key={sidebarItemChild.id}
+                            className="w-full"
                           >
-                            <div className="text-white group-hover:text-black font-body font-medium text-title_3">
-                              {sidebarItemChild.title}
-                            </div>
-                          </AccordionTrigger>
-                          {sidebarItemChild.subChildren?.map(
-                            (sidebarItemChildSubChild) => {
-                              return (
-                                <AccordionContent
-                                  key={sidebarItemChildSubChild.id}
-                                  className="text-title_3"
+                            <AccordionTrigger
+                              icon={
+                                <React.Suspense
+                                  fallback={
+                                    <div className="w-4 h-4 bg-stroke/60 animate-pulse"></div>
+                                  }
                                 >
-                                  <div className="hover:cursor-pointer hover:bg-white hover:text-black font-body text-white font-medium w-full py-2 pl-8 first:mt-3">
-                                    {sidebarItemChildSubChild.title}
-                                  </div>
-                                </AccordionContent>
-                              );
-                            }
-                          )}
-                        </AccordionItem>
-                      );
-                    })}
-                  </Accordion>
-                )}
-              </div>
-            );
-          })}
+                                  <ExpandMoreIcon className="text-white/50 group-hover:text-black/50 ease-[cubic-bezier(0.87,_0,_0.13,_1)] transition-transform duration-300 group-data-[state=open]:rotate-180" />
+                                </React.Suspense>
+                              }
+                              headerClassName="w-full"
+                              className="px-4 py-2 group hover:cursor-pointer hover:bg-white w-full flex items-center justify-between"
+                            >
+                              <div className="text-white group-hover:text-black font-body font-medium text-title_3">
+                                {sidebarItemChild.title}
+                              </div>
+                            </AccordionTrigger>
+                            {sidebarItemChild.subChildren?.map(
+                              (sidebarItemChildSubChild) => {
+                                return (
+                                  <AccordionContent
+                                    key={sidebarItemChildSubChild.id}
+                                    className="text-title_3"
+                                  >
+                                    <div
+                                      className="hover:cursor-pointer hover:bg-white hover:text-black font-body text-white font-medium w-full py-2 pl-8 first:mt-3"
+                                      onClick={() => setShowSidebar(false)}
+                                    >
+                                      {sidebarItemChildSubChild.title}
+                                    </div>
+                                  </AccordionContent>
+                                );
+                              }
+                            )}
+                          </AccordionItem>
+                        );
+                      })}
+                    </Accordion>
+                  )}
+                </div>
+              );
+            })}
+          </div>
         </div>
       </>
     );
