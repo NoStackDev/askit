032e69e0ef6fdcf944fb41868cd058ba85b3cf0a
Ewomazino Osiogbo <77966274+NoStackDev@users.noreply.github.com>
2023-07-05 22:46:36 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -86,6 +86,7 @@ export default function Home() {
   };
 
   return (
+    <>
     <main className="relative md:mr-[100px] md:ml-[60px]">
       <div className="relative flex md:gap-[92px] md:mb-10">
         <div className="mx-5 my-6 mt-[24px] md:mt-[56px] w-full z-10">
@@ -190,5 +191,6 @@ export default function Home() {
         <RequestForm className="" />
       </Dialog>
     </main>
+      </>
   );
 }
