97f93a68a30985d9085a51c583a23dbc89cf9897
NoStackDev <partialstackdev@gmail.com>
2023-07-17 16:41:01 +0100

    update

--- src/app/requests/[id]/section/RequestImgDetail.tsx
+++ src/app/requests/[id]/section/RequestImgDetail.tsx
@@ -205,9 +205,7 @@ const RequestImgDetail = React.forwardRef<
               height={24}
               alt="report"
             />
-            <div className="text-title_3 font-body text-black/60">
-              Report
-            </div>
+            <div className="text-title_3 font-body text-black/60">Report</div>
           </div>
 
           <div className="flex items-center gap-1 hover:cursor-pointer">
@@ -228,9 +226,7 @@ const RequestImgDetail = React.forwardRef<
                 />
               )}
             </React.Suspense>
-            <div className="text-title_3 font-body text-black/60">
-              Save
-            </div>
+            <div className="text-title_3 font-body text-black/60">Save</div>
           </div>
 
           <div className="flex items-center gap-1 hover:cursor-pointer">
@@ -241,133 +237,9 @@ const RequestImgDetail = React.forwardRef<
             >
               <ShareIcon className="text-black/60" />
             </React.Suspense>
-            <div className="text-title_3 font-body text-black/60">
-              Share
-            </div>
+            <div className="text-title_3 font-body text-black/60">Share</div>
           </div>
         </div>
-
-        {/* {image_url && (
-          <Image
-            src={`https://${image_url}`}
-            height={255}
-            width={255}
-            alt="requested product image"
-            className="max-w-[235px] max-h-[300px] md:max-w-[255px]"
-          />
-        )}
-
-        <div className="h-full w-full">
-          <div className="mt-4">
-            <div className="font-body text-title_1 font-medium text-left">
-              {title}
-            </div>
-
-            <div className="mt-[19px] md:mt-3 flex items-center gap-4">
-              <div className="flex gap-1 items-center">
-                <div className="font-body text-special text-[#000000] font-light h-full">
-                  Location needed:
-                </div>
-                <div className="font-body text-special text-[#000000] font-medium">
-                  {location}
-                </div>
-              </div>
-
-              <div>
-                <React.Suspense
-                  fallback={
-                    <div className="w-10 h-4 bg-stroke/80 animate-pulse"></div>
-                  }
-                >
-                  <div className="flex items-center gap-1">
-                    <WatchLaterIcon
-                      className="text-[#ADABAB]"
-                      fontSize="small"
-                    />
-                    <span className="text-[#000000]/60 font-body text-special">
-                      {date.getDay()} {month(date.getMonth())}
-                    </span>
-                  </div>
-                </React.Suspense>
-              </div>
-            </div>
-
-            <div className="flex flex-col justify-between w-full">
-              <div className="mt-6 md:mt-4 flex flex-col gap-2">
-                <div className="font-body text-title_3 text-secondary font-medium">
-                  More details
-                </div>
-                <div className="font-body text-body_1">{description}</div>
-              </div>
-
-              <div className="mt-7 md:mt-11 flex items-center justify-between md:gap-10 w-full">
-                <div className="flex items-center gap-1">
-                  <React.Suspense
-                    fallback={
-                      <div className="w-[18px] h-3 bg-stroke/80 animate-pulse"></div>
-                    }
-                  >
-                    <VisibilityIcon className="text-stroke" />
-                  </React.Suspense>
-                  <div className="text-special font-body font-light text-[#000000]">
-                    200 views
-                  </div>
-                </div>
-
-                <div className="flex justify-between items-center w-full ">
-                  <div className="flex items-center gap-1 hover:cursor-pointer">
-                    <React.Suspense
-                      fallback={
-                        <div className="w-[18px] h-4 bg-stroke/80 animate-pulse"></div>
-                      }
-                    >
-                      <FlagIcon className="text-black" />
-                    </React.Suspense>
-                    <div className="text-title_3 font-body font-medium text-black">
-                      Report
-                    </div>
-                  </div>
-
-                  <div className="flex items-center gap-1 hover:cursor-pointer">
-                    <React.Suspense
-                      fallback={
-                        <div className="w-4 h-4 bg-stroke/80 animate-pulse"></div>
-                      }
-                    >
-                      {bookmarked ? (
-                        <BookmarkIcon
-                          className="text-primary hover:cursor-pointer"
-                          onClick={onBookmarkClick}
-                        />
-                      ) : (
-                        <BookmarkBorderIcon
-                          className="text-primary hover:cursor-pointer"
-                          onClick={onBookmarkClick}
-                        />
-                      )}
-                    </React.Suspense>
-                    <div className="text-title_3 font-body font-medium text-black">
-                      Save
-                    </div>
-                  </div>
-
-                  <div className="flex items-center gap-1 hover:cursor-pointer">
-                    <React.Suspense
-                      fallback={
-                        <div className="w-[18px] h-5 bg-stroke/80 animate-pulse"></div>
-                      }
-                    >
-                      <ShareIcon className="text-black" />
-                    </React.Suspense>
-                    <div className="text-title_3 font-body font-medium text-black">
-                      Share
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div> */}
       </div>
     );
   }
